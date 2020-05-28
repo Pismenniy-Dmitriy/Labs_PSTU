277e4c83ecf64f290c5b6594c20c9ea2a5246dc6
Pismenniy-Dmitriy <64082352+Pismenniy-Dmitriy@users.noreply.github.com>
2020-05-29 00:20:13 +0500

    Add files via upload

---
+++ 22lab(Перегрузка операций)/Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,18 @@
+﻿#include"Time.h"
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	Time a;
+	Time b;
+	Time c;
+	cin >> a;
+	cin >> b;
+	++a;
+	cout << a << endl;
+	c = (a++) + b;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	cout <<"c=" << c << endl;
+}
