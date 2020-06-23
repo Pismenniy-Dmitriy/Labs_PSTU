1c625341b447fb4fe8c605746698453ac54f0315
Pismenniy-Dmitriy <64082352+Pismenniy-Dmitriy@users.noreply.github.com>
2020-06-24 01:25:25 +0500

    Add files via upload

---
+++ 32lab(коммивояжер)/Source.cpp
@@ -0,0 +1,23 @@
+﻿#include "Graph.h"
+#include <glut.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+int main(int argc, char* argv[])
+{
+	
+	glutInit(&argc, argv);
+	graph = makeGraph();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1350, 730);
+	glutCreateWindow("Graph");
+	WinW = glutGet(GLUT_WINDOW_WIDTH);
+	WinH = glutGet(GLUT_WINDOW_HEIGHT);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouseClick);
+	glutMainLoop();
+	return 0;
+}
